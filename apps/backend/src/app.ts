70e74c108365b9499d597434cb02c9558a47a436
Humbe jeffrey <humbejeff.dev@gmail.com>
2026-01-10 05:00:52 +0100

    Breaking change: remove live router

--- apps/backend/src/app.ts
+++ apps/backend/src/app.ts
@@ -1,13 +1,7 @@
 import express from "express";
 import cors from "cors";
-import { startWebSocketServer } from "./websocket/wsServer";
-import { liveRouter } from "./api/live.controller";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/live", liveRouter);
-
-// startWebSocketServer(httpServer);
