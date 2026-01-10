f88e70c3f4e4f3f4efe3b0186d0a5aefc369ef1f
Humbe jeffrey <humbejeff.dev@gmail.com>
2026-01-10 05:25:57 +0100

    attach WebSocketProvider and ConnectionToast components

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { DebugProvider } from './state/DebugStateProvider.tsx';
+import { WebSocketProvider } from './state/WebSocketStateProvider.tsx';
+import { ConnectionToast } from './components/ConnectionToast.tsx';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DebugProvider>
-      <App />
+      <WebSocketProvider>
+        <ConnectionToast />
+        <App />
+      </WebSocketProvider>
     </DebugProvider>
   </StrictMode>
 );
