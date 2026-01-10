bd5f2d9324eed4ecc4547e664a335c803a773269
Humbe jeffrey <humbejeff.dev@gmail.com>
2026-01-10 05:07:25 +0100

    add audioModel property to geminiLiveConfig object

--- apps/backend/src/gemini/live/liveConfig.ts
+++ apps/backend/src/gemini/live/liveConfig.ts
@@ -1,14 +1,19 @@
+import {
+  Modality,
+} from "@google/genai";
 
 export const geminiLiveConfig = {
-  model: "models/gemini-2.0-flash-live-preview",
+  model: "gemini-2.0-flash-live-preview",
+  audioModel: "gemini-2.5-flash-native-audio-preview-09-2025",
   generationConfig: {
     temperature: 0.3,
     maxOutputTokens: 512,
+    systemInstruction: `
+    You are a calm, patient math teacher.
+    You teach linear equations step by step.
+    You adapt explanations based on student confusion.
+    Never give the final answer immediately.
+    `,
+    // responseModalities: [Modality.AUDIO],
   },
-  systemInstruction: `
-  You are a calm, patient math teacher.
-  You teach linear equations step by step.
-  You adapt explanations based on student confusion.
-  Never give the final answer immediately.
-  `,
 };
