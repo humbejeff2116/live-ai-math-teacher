3fafcd8eef8c02c6bfd3c65e77cb6945d38949f7
Humbe Jeffrey <humbejeff2116@gmail.com>
2026-01-22 13:51:24 +0100

    switch back to BrowserRouter

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -4,8 +4,8 @@ import './index.css'
 import AppRoutes from "./App.tsx";
 import { DebugProvider } from './state/DebugStateProvider.tsx';
 import { WebSocketProvider } from './state/WebSocketStateProvider.tsx';
-// import { BrowserRouter as Router } from "react-router-dom";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+// import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
